5d9818b5e046da6f2be545be0fbbe1457cf4708b
Edward Z. Yang <edwardzyang@thewritingpot.com>
2008-11-02 03:39:10 -0500

    Release 1.0.1.
    Signed-off-by: Edward Z. Yang <edwardzyang@thewritingpot.com>

--- csrf-magic.php
+++ csrf-magic.php
@@ -129,7 +129,7 @@ $GLOBALS['csrf']['xhtml'] = true;
 // FUNCTIONS:
 
 // Don't edit this!
-$GLOBALS['csrf']['version'] = '1.0.0';
+$GLOBALS['csrf']['version'] = '1.0.1';
 
 /**
  * Rewrites <form> on the fly to add CSRF tokens to them. This can also
